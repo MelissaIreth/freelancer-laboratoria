ed481a42f126c03e0e0ec0d42406bc0cbc81a2d1
Melissa Pacheco <melissa.pacheco.martinez@gmail.com>
2017-06-01 10:36:18 -0400

    modal and close

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
 /*
  * Archivo principal de JS
  */
+ (function manin(){
+ 	var box = Array.from(document.getElementsByClassName("caja"));
+ 	var modal = document.getElementById("row-modal");
+ 	var bodyModal, close ,i;
+
+ 	box.forEach(function(boxes){
+ 		boxes.addEventListener("click", function(){
+ 			modal.innerHTML = "";
+ 			bodyModal = document.createElement("div");
+ 			bodyModal.classList.add("body-modal");
+ 			bodyModal.innerHTML = boxes.innerHTML;
+ 			modal.appendChild(bodyModal);
+ 			modal.classList.remove("hide");
+ 			close = document.createElement("div");
+ 			close.classList.add("close");
+ 			i = document.createElement("i");
+ 			i.classList.add("fa", "fa-times-circle")
+ 			i.setAttribute("aria-hidden", "true")
+ 			close.appendChild(i);
+ 			modal.appendChild(close);
+ 			close.addEventListener("click", function(){
+ 				modal.classList.add("hide");
+ 			});
+ 		});
+ 	});
+ })();
